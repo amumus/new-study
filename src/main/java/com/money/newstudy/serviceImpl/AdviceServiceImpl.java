4705b5efa778577b20f99bccbf3e7fbd1adc5644
amumus <1013760411@qq.com>
2019-05-01 15:30:38 +0800

    登录接口

--- src/main/java/com/money/newstudy/serviceImpl/AdviceServiceImpl.java
+++ src/main/java/com/money/newstudy/serviceImpl/AdviceServiceImpl.java
@@ -4,7 +4,9 @@ import com.money.newstudy.biz.autoCode.mapper.AdviceMapper;
 import com.money.newstudy.biz.autoCode.pojo.Advice;
 import com.money.newstudy.service.AdviceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AdviceServiceImpl implements AdviceService {
 
     @Autowired
